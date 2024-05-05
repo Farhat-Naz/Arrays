d2356d7aa3e2da4d20b81e087f7ec82e04acf3ec
Farhat Naz <159726939+Farhat-Naz@users.noreply.github.com>
2024-05-06 02:23:35 +0500

    Add files via upload

---
+++ array.ts
@@ -0,0 +1,16 @@
+//Arrays
+let sampleArray:string[]=["shareq","Mishal","safan","kamran","Aslam"];
+console.log(sampleArray);
+// sampleArray.push("Farhat");
+// console.log(sampleArray);
+// sampleArray.unshift("Zargham");
+// console.log(sampleArray);
+// sampleArray.pop();
+// console.log(sampleArray);
+// sampleArray.shift();
+// console.log(sampleArray);
+// let size=sampleArray.length;
+// console.log(size);
+//sampleArray.filter()
+let newArray=sampleArray.slice(3);
+console.log(newArray);
